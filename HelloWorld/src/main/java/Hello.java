68aead67e64e9cfd1dd9da8c792410dacf2ffc53
Landarby Vincent <vincentlandarby@gmail.com>
2022-06-14 08:43:59 -0400

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author Landarby Vincent
+ *email: vincentlandarby@gmail.com
+ *date: 06/14/2022
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.lv.helloworld; 
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Landarby!");
+    }
+}
